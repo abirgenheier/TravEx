2e9a90ba05a2e65339b8dc77270e936bb35c551a
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-06-09 09:44:18 -0700

    update

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.json())
 
 app.use(express.static("public"))
 
-require("./app/routes/static-api-routes")(app)
-require("./app/routes/dynamic-api-routes")(app)
-require("./app/routes/html-routes")(app)
+require("./routes/static-api-routes")(app)
+require("./routes/dynamic-api-routes")(app)
+require("./routes/html-routes")(app)
 
 db.sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
